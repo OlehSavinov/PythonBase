6b4756fca1912884deb896e610be2d8001ccd219
admin <olegsavinov12@gmail.com>
2020-02-03 18:29:15 +0200

    hw

--- hw_3_5.py
+++ hw_3_5.py
@@ -28,12 +28,23 @@ def get_vocabluary(text):
         result[i] = input_translate(i)
     return result
 
+def translate(text, dictionary):
+    # result = text
+    for i in punctuation:
+        text = text.replace(i, ' ')
+    spl_text = text.split()
+    for k in spl_text:
+
+        spl_text[i] =
+    return spl_text
+
 if __name__ == "__main__":
-    TEXT = 'привет, как дела!?'
+    TEXT = 'Привет и как привет///.. дела????'
     vocabluary = get_vocabluary(TEXT)
-    print('\nСформированный словарь переводов слов из текста:')
+    print('\nСформированный словарь переводов слов из текста (перевод упорядочен по алфавиту):')
     l_voc = list(vocabluary.items())
     l_voc.sort(key=lambda i: i[1])
     for key, value in l_voc:
         res = '| {0:<{1}} | {2:>{3}} |'.format(key, max_len(vocabluary.keys()), value, max_len(vocabluary.values()))
         print(res)
+    print(translate(TEXT, vocabluary))
